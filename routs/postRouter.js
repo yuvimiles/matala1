362b4723f794fefaf11fdd246c02041a239f98e6
yuval miles <yuvalmil9@gmail.com>
2025-01-16 17:26:53 +0200

    post by id - done

--- routs/postRouter.js
+++ routs/postRouter.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const postController = require("../controller/PostsController.js")
 
 router.get("/All",postController.getAllposts)
-router.get("/", postController.getPostBySender )
+
+router.get("/", postController.getPostBySender)
+
+router.get("/:id", postController.getPost)
+
 module.exports = router;
